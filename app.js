16deeeab1c5e241d646ffc2d14dc551ce15adb80
jtmar28 <justinmartinez0123@gmail.com>
2018-11-28 22:37:27 -0500

    Delete book from list using prototype object

--- app.js
+++ app.js
@@ -1,23 +1,31 @@
-function Book(title,isbn, name){
+function Book(title,isbn, author){
     this.title = title;
     this.isbn = isbn;
-    this.name = name;
+    this.author = author;
 }
 function UI(){}
 
 UI.prototype.addBookToList = function(book){
+    const list = document.querySelector('#book-list');
     const tr = document.createElement('tr');
     tr.innerHTML = `
         <td>${book.title}</td>
-        <td>${book.isbn}</td>
         <td>${book.author}</td>
+        <td>${book.isbn}</td>
         <td><a href="#" class="delete">X</a></td>
     `;
-    const list = document.querySelector('#book-list');
     list.appendChild(tr);
-    e.preventDefault();
 }
-
+UI.prototype.clearFields = function(){
+    document.querySelector('#title').value = "";
+    author = document.querySelector('#author').value = "";
+    isbn = document.querySelector('#isbn').value = "";
+}
+UI.prototype.deleteBookFromList = function(target){
+    if(target.classList.contains('delete')){
+        target.parentElement.parentElement.remove();
+    }
+}
 document.querySelector('#book-form').addEventListener('submit', function(e){
     const title = document.querySelector('#title').value,
           author = document.querySelector('#author').value,
@@ -25,5 +33,10 @@ document.querySelector('#book-form').addEventListener('submit', function(e){
     const book = new Book(title,author,isbn);
     const ui = new UI();
     ui.addBookToList(book);
+    ui.clearFields();
     e.preventDefault();
+});
+document.querySelector('#book-list').addEventListener('click', function(e){
+    const ui = new UI();
+    ui.deleteBookFromList(e.target);
 });
