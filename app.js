d0f574f1f3185f8f67cb0144c51419c17721330e
jtmar28 <justinmartinez0123@gmail.com>
2018-11-28 22:09:30 -0500

    Function to add book to list using prototype objects

--- app.js
+++ app.js
@@ -4,3 +4,26 @@ function Book(title,isbn, name){
     this.name = name;
 }
 function UI(){}
+
+UI.prototype.addBookToList = function(book){
+    const tr = document.createElement('tr');
+    tr.innerHTML = `
+        <td>${book.title}</td>
+        <td>${book.isbn}</td>
+        <td>${book.author}</td>
+        <td><a href="#" class="delete">X</a></td>
+    `;
+    const list = document.querySelector('#book-list');
+    list.appendChild(tr);
+    e.preventDefault();
+}
+
+document.querySelector('#book-form').addEventListener('submit', function(e){
+    const title = document.querySelector('#title').value,
+          author = document.querySelector('#author').value,
+          isbn = document.querySelector('#isbn').value;
+    const book = new Book(title,author,isbn);
+    const ui = new UI();
+    ui.addBookToList(book);
+    e.preventDefault();
+});
